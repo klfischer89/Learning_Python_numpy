21257953acd76ed552da1d2c47d4fef7f6b016cb
klfischer89 <fischer-k4@web.de>
2025-11-24 16:16:41 +0100

    create matrix and access rows, columns and elements

--- introduction_numpy.py
+++ introduction_numpy.py
@@ -1,9 +1,9 @@
 import numpy as np # import numpy
 import matplotlib.pyplot as plt
 
-x = np.array(range(1,11)) # list from 1 to 10 using numpy array
+# x = np.array(range(1,11)) # list from 1 to 10 using numpy array
 
-y = x ** 2 - 4 # squre x and substract 4 for each element
+# y = x ** 2 - 4 # squre x and substract 4 for each element
 
 # print(y) # print calculated elements
 
@@ -12,18 +12,27 @@ y = x ** 2 - 4 # squre x and substract 4 for each element
 
 # print(np.arange(1,10,1)) 
 
-xs = np.array([1,5,9]) # define two arrays(vectors)
-a = np.arange(1,5)[:3] # slice array to have same shape as xs 
+# xs = np.array([1,5,9]) # define two arrays(vectors)
+# a = np.arange(1,5)[:3] # slice array to have same shape as xs 
 # print(xs + a) # add two vectors
 # print(xs <= a) # check element wise
 
 # plt.plot([1,2,3], [6,7,8]) # create x and y values
 # plt.show() # show plot
 
-xv = np.arange(-3, 3, 0.1)
-yv = xv ** 3 - xv ** 2 + 6
+# xv = np.arange(-3, 3, 0.1)
+# yv = xv ** 3 - xv ** 2 + 6
 
 # plt.scatter(xv, yv)
 # plt.plot(xv, yv)
 # plt.show()
 
+xs = np.array([ # create matrix
+    [1, 2, 3, 4, 5], 
+    [1, 2, 3, 4, 5], 
+    [4, 5, 6, 7, 8]])
+
+print(xs.shape) # print shape of xs
+print(xs[0]) # access first row of the matrix
+print(xs[:,0]) # access first column
+print(xs[2,4]) # access element in thrid row and fifth column 
